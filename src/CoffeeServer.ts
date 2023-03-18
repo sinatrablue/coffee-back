80c590fc9b18992bff06a85ec5c73be79fcec4b1
Robin Simonot <simonot.robin@gmail.com>
2023-03-18 16:56:25 +0100

    constructor and beggining of start method

--- src/CoffeeServer.ts
+++ src/CoffeeServer.ts
@@ -1,10 +1,26 @@
 import { CoffeeServerOptions } from "./interfaces/CoffeeServerOptions";
+import { api } from "./api";
+import express, { Express } from "express";
 
 export class CoffeeServer {
-    constructor(options?: CoffeeServerOptions) {}
+    app: Express
+    options: CoffeeServerOptions = {
+        port: 3000
+    }
+
+    constructor(options?: CoffeeServerOptions) {
+        if (options)
+            this.options = options
+        
+        const app = express();
+        app.use("/api", api)
+        this.app = app
+    }
 
     async start() {
-        throw new Error("Not implemented")
+        this.app.listen(this.options.port, () => {
+            console.log(` > Started on port : ${this.options.port}`)
+        })
     }
 
     async stop() {
--- test/coffeeserver.spec.ts
+++ test/coffeeserver.spec.ts
@@ -6,10 +6,10 @@ describe("CoffeeServer", () => {
     it("should start & stop", async () => {
         const coffeeServer = new CoffeeServer({ port: 3000} )
         await coffeeServer.start()
-        let response = await axios.get("localhost:3000/api/ping");
+        let response = await axios.get("http://localhost:3000/api/ping");
         assert.equal(response.status, 200)
         await coffeeServer.stop()
-        response = await axios.get("localhost:3000/api/ping");
+        response = await axios.get("http://localhost:3000/api/ping");
         assert.equal(response.status, 0)
     })
     it("should be ok", () => {
